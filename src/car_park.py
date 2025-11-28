03e413a4c3505fadab3d2da58b6e481590d3a991
Bwin <20135272@tafe.wa.edu.au>
2025-11-28 09:16:56 +0800

    CarPark Class has had attributes added

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, capacity = 100, plates = None, displays = None, location = "Unknown", ):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.displays = displays or []
+
+    def __str__(self):
+        return f"Car park at {self.location} with {self.capacity} bays"
